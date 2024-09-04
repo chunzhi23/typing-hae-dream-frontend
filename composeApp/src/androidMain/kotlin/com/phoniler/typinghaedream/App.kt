f04ebc02a3077e4593d733bce394c4dea64b8f53
chunzhi23 <05.huanx10ng@gmail.com>
2024-09-04 10:51:32 +0900

    [FEAT] Color theme
    Simultaneous change function added

--- composeApp/src/androidMain/kotlin/com/phoniler/typinghaedream/App.kt
+++ composeApp/src/androidMain/kotlin/com/phoniler/typinghaedream/App.kt
@@ -1,36 +1,28 @@
 package com.phoniler.typinghaedream
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.jetbrains.compose.resources.painterResource
+import com.phoniler.typinghaedream.theme.ThemeColors
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import typinghaedream.composeapp.generated.resources.Res
-import typinghaedream.composeapp.generated.resources.compose_multiplatform
 
-@Composable
 @Preview
-fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
-            }
-            AnimatedVisibility(showContent) {
-                val greeting = remember { Greeting().greet() }
-                Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
-                    Image(painterResource(Res.drawable.compose_multiplatform), null)
-                    Text("Compose: $greeting")
-                }
-            }
-        }
+@Composable
+fun App(themeColors: ThemeColors) {
+    Box(
+        modifier =
+            Modifier
+                .fillMaxSize()
+                .background(themeColors.backgroundColor)
+    ) {
+        Text(
+            text = "Hello, Multiplatform!",
+            color = themeColors.textColor,
+            modifier = Modifier.align(Alignment.Center)
+        )
     }
 }
