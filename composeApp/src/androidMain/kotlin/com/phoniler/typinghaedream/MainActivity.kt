1563485ecea345088ea9d0ce666d146469a23a40
chunzhi23 <05.huanx10ng@gmail.com>
2024-09-04 11:04:43 +0900

    [FEAT] Color theme
    Simultaneous change function added
    
    (cherry picked from commit f04ebc02a3077e4593d733bce394c4dea64b8f53)

--- composeApp/src/androidMain/kotlin/com/phoniler/typinghaedream/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/phoniler/typinghaedream/MainActivity.kt
@@ -1,23 +1,39 @@
 package com.phoniler.typinghaedream
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalConfiguration
+import com.phoniler.typinghaedream.theme.getThemeColors
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            App()
+            ThemedApp()
         }
     }
 }
 
-@Preview
 @Composable
-fun AppAndroidPreview() {
-    App()
+fun ThemedApp() {
+    // Observe the current configuration
+    val configuration = LocalConfiguration.current
+
+    // Determine if the system is in dark mode
+    val isDarkTheme =
+        when (configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) {
+            Configuration.UI_MODE_NIGHT_YES -> true
+            Configuration.UI_MODE_NIGHT_NO -> false
+            else -> false
+        }
+
+    // Get the theme colors based on the current theme
+    val themeColors = getThemeColors(isDarkTheme)
+
+    // Use the theme colors
+    App(themeColors = themeColors)
 }
---
+++ shared/src/commonMain/kotlin/com/phoniler/typinghaedream/theme/ThemeColors.kt
@@ -0,0 +1,23 @@
+package com.phoniler.typinghaedream.theme
+
+import androidx.compose.ui.graphics.Color
+
+data class ThemeColors(
+    val primaryColor: Color,
+    val onPrimaryColor: Color,
+    val secondaryColor: Color,
+    val onSecondaryColor: Color,
+    val primaryTextColor: Color,
+    val textColor: Color,
+    val descColor: Color,
+    val strokeColor: Color,
+    val dividerColor: Color,
+    val regionColor: Color,
+    val backgroundColor: Color,
+    val blackColor: Color,
+    val whiteColor: Color,
+    val regionEditorColor: Color,
+    val selectedColor: Color,
+    val dangerColor: Color,
+    val approvedColor: Color
+)
